17963211c8edb6ef0aa13a00e8250f26b9354243
Pedro Lima <pedro.l2002@hotmail.com>
2023-03-17 00:12:01 -0300

    feat: captura dupla implementada, todo: corrigir alguns bugs

--- checkers.py
+++ checkers.py
@@ -1,6 +1,9 @@
 from no import No
 import time
 
+class Possibilidade:
+    pass
+
 class Peca:
     # Tipos
     # N - Normal
@@ -29,9 +32,11 @@ class Peca:
         return f"{self.emoji}"
     
 class Casa:
-    def __init__(self, cor, peca: Peca = None):
+    def __init__(self, cor, linha, coluna, peca: Peca = None):
         self.cor = cor
         self.peca = peca
+        self.linha = linha
+        self.coluna = coluna
     
     def __repr__(self):
         if self.peca:
@@ -51,25 +56,25 @@ class Checkers:
             self.table.append([])
             for j in range(8):
                 if (i == 5 or i == 7) and j % 2 == 0:
-                    self.table[i].append(Casa(cor="P", peca=Peca("N", "B")))
+                    self.table[i].append(Casa(cor="P",linha = i, coluna=j, peca=Peca("N", "B")))
                 elif i == 6 and j % 2 == 1:
-                    self.table[i].append(Casa(cor="P", peca=Peca("N", "B")))
+                    self.table[i].append(Casa(cor="P",linha = i, coluna=j, peca=Peca("N", "B")))
                 elif (i == 0 or i == 2) and j % 2 == 1:
-                    self.table[i].append(Casa(cor="P", peca=Peca("N", "P")))
+                    self.table[i].append(Casa(cor="P",linha = i, coluna=j, peca=Peca("N", "P")))
                 elif i == 1 and j % 2 == 0:
-                    self.table[i].append(Casa(cor="P", peca=Peca("N", "P")))
+                    self.table[i].append(Casa(cor="P",linha = i, coluna=j, peca=Peca("N", "P")))
                 elif i == 3:
                     if j % 2 == 1:
-                        self.table[i].append(Casa(cor="B"))
+                        self.table[i].append(Casa(cor="B",linha = i, coluna=j))
                     else:
-                        self.table[i].append(Casa(cor="P"))
+                        self.table[i].append(Casa(cor="P",linha = i, coluna=j))
                 elif i == 4:
                     if j % 2 == 0:
-                        self.table[i].append(Casa(cor="B"))
+                        self.table[i].append(Casa(cor="B",linha = i, coluna=j))
                     else:
-                        self.table[i].append(Casa(cor="P"))
+                        self.table[i].append(Casa(cor="P",linha = i, coluna=j))
                 else: 
-                    self.table[i].append(Casa(cor="P"))
+                    self.table[i].append(Casa(cor="P", linha = i, coluna=j))
     
     def print(self):
         for linha in range(len(self.table)):
@@ -93,7 +98,8 @@ class Checkers:
 
         return True
     
-
+    def calcular_possibilidades(self, destino):
+        return [[destino[0] + 1, destino[1] + 1], [destino[0] + 1, destino[1] - 1], [destino[0] - 1, destino[1] + 1] ,[destino[0] - 1, destino[1] - 1]]
     def valida_fim_de_jogo(self, no = None):
 
         if no == None:
@@ -130,45 +136,66 @@ class Checkers:
         #Captura
         while (casa_destino.peca != None):
             captura = False
-            possibilidades = [[destino[0] + 1][destino[1] + 1], [destino[0] + 1][destino[1] - 1], [destino[0] - 1][destino[1] + 1] ,[destino[0] - 1][destino[1] - 1]]
             possibilidade_utilizada = None
             #valida se não está capturando a própria peça
             if  (casa_destino.peca.cor == jogador):
                 print("Movimento inválido!")
                 return None
+            elif destino[0] + 1 >= len(estado) or destino[1] + 1 >= len(estado):
+                break
             elif (estado[destino[0] + 1][destino[1] + 1].peca == None and origem[0] < destino[0] and origem[1] < destino[1]):
                 estado[destino[0]][destino[1]].peca = None
                 destino = [destino[0] + 1, destino[1] + 1]
-                possibilidade_utilizada = [x for x in possibilidades if destino == x][0]
                 captura = True
             elif (estado[destino[0] + 1][destino[1] - 1].peca == None and origem[0] < destino[0] and origem[1] > destino[1]):
                 estado[destino[0]][destino[1]].peca = None
                 destino = [destino[0] + 1, destino[1] - 1]
-                possibilidade_utilizada = [x for x in possibilidades if destino == x][0]
                 captura = True
 
             elif (estado[destino[0] - 1][destino[1] + 1].peca == None and origem[0] > destino[0] and origem[1] < destino[1]):
                 estado[destino[0]][destino[1]].peca = None
                 destino = [destino[0] - 1, destino[1] + 1]
-                possibilidade_utilizada = [x for x in possibilidades if destino == x][0]
-
                 captura = True
+
             elif (estado[destino[0] - 1][destino[1] - 1].peca == None and origem[0] > destino[0] and origem[1] > destino[1]):
                 estado[destino[0]][destino[1]].peca = None
                 captura = True
                 destino = [destino[0] - 1, destino[1] - 1]
-                possibilidade_utilizada = [x for x in possibilidades if destino == x][0]
 
-            peca_antiga = estado[origem[0]][origem[1]].peca
+
+            possibilidades = self.calcular_possibilidades(destino)
+            nova_casa_origem = estado[destino[0]][destino[1]]         
+            casas_possiveis = []
+            for possibilidade in possibilidades:
+                casa_a_ser_testada = estado[possibilidade[0]][possibilidade[1]]
+                if casa_a_ser_testada.peca != None and casa_a_ser_testada.peca.cor != jogador:
+                    diff_linha = casa_a_ser_testada.linha - nova_casa_origem.linha
+                    diff_coluna = casa_a_ser_testada.coluna - nova_casa_origem.coluna
+                    if diff_linha + casa_a_ser_testada.linha >= len(estado) or diff_linha + casa_a_ser_testada.coluna >= len(estado):
+                        break
+                    if estado[possibilidade[0] + diff_linha][possibilidade[1] + diff_coluna].peca == None:
+                        casas_possiveis.append(casa_a_ser_testada)
+
+
+            if len(casas_possiveis) == 1:
+                casa_destino = casas_possiveis[0]
+            else:
+                print("Movimentos possíveis:")
+                for movimento in casas_possiveis:
+                    print(f"{casas_possiveis.index(movimento) + 1} Linha: {movimento.linha} - Coluna: {movimento.coluna}")
+                opt = int(input("Escolha uma opção: "))
+                casa_destino = casas_possiveis[opt - 1]
+
+            peca_antiga = casa_origem.peca
 
             if(casa_origem.peca.cor == "P" and destino[0] == 7) or (casa_origem.peca.cor == "B" and destino[0] == 0):
                 estado[destino[0]][destino[1]].peca = Peca(tipo="D", cor=peca_antiga.cor)
             else:
                 estado[destino[0]][destino[1]].peca = casa_origem.peca
             estado[origem[0]][origem[1]].peca = None
-
-            casa_origem = casa_destino
-            casa_destino = estado[possibilidade_utilizada[0]][possibilidade_utilizada[1]]         
+            destino = [casa_a_ser_testada.linha, casa_a_ser_testada.coluna]
+            origem = [nova_casa_origem.linha, nova_casa_origem.coluna]
+            casa_origem = nova_casa_origem
         
         if not captura:
             peca_antiga = estado[origem[0]][origem[1]].peca
