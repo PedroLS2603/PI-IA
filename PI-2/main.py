132d40103ab307814107eff0cda843c5a948d059
Pedro Lima <pedro.l2002@hotmail.com>
2023-04-29 20:19:31 -0300

    feat: restringindo canal do servidor

--- PI-2/main.py
+++ PI-2/main.py
@@ -1,9 +1,13 @@
 import discord
 import os
+import json
  
 intents = discord.Intents.all()
 client = discord.Client(command_prefix='!', intents=intents)
- 
+
+util = json.load(open("util.json"))
+
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -12,8 +16,11 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    
-    if len(message.attachments) > 0:
-        await message.channel.send('Imagem recebida!')
+
+    if util["channel_id"] == str(message.channel.id):
+        if len(message.attachments) > 0:
+            await message.channel.send('Imagem recebida!')
+
+        print(message.content)
         
 client.run(os.getenv('DISCORD-TOKEN'))
